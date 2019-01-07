f0e67ec3173ede7f0665dd05adc89533b6c0f30e
Regine Laguting <=>
2019-01-07 23:27:27 +0800

    css

--- resources/views/components/home-navbar.blade.php
+++ resources/views/components/home-navbar.blade.php
@@ -3,40 +3,31 @@
     <!-- Navbar Logo -->
     <div class="logo">
       <!-- Logo Placeholder for Inlustration -->
-      <img src="{{ asset('images/logo.png') }}" style="float: left;width:50px;height: auto; margin-top: 5px;"> 
+      <img src="{{ asset('images/logo.png') }}", id="logo"> 
+      <!-- <p id="fiercecom-text">Fiercecom Inc.</p> -->
+      <a href="#notification"><img src="{{asset('images/bellicon.png')}}", id="img-icon"></a>
     </div>
 
     <!-- Navbar Links -->
    
-    <ul id="menu">
+    <ul id="menu"> 
       <li><a href="javascript:unhide('toon1');">Apply</a></li>
-	   <li><a href="#services">Services</a></li>
-	   <li><a href="#about">About</a></li>
-	   <li><a href="#contact">Contact</a></li>
-     <li>
-      <a href="{{ route('logout') }}"
-          onclick="event.preventDefault();
-                  document.getElementById('logout-form').submit();">
-          Logout
-      </a>
-      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-          {{ csrf_field() }}
-      </form>
-     </li>
+     <li><a href="#services">Services</a></li>
+     <li><a href="#about">About</a></li>
+     <li><a href="#contact">Contact</a></li>
     </ul>
-                                        
 </nav>
 
    
 <!-- Menu Icon -->
-<!-- <div class="menuIcon">
+<div class="menuIcon">
   <span class="icon icon-bars"></span>
   <span class="icon icon-bars overlay"></span>
-</div> -->
+</div>
 
 <div class="overlay-menu">
   <ul id="menu">
-      <li><a  >Apply</a></li>
+      <li><a href="javascript:unhide('toon1');">Apply</a></li>
       <li><a href="#services">Services</a></li>
       <li><a href="#about">About</a></li>
       <li><a href="#contact">Contact</a></li>
