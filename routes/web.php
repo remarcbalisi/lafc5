2a34f811df3c9a0737fd2ec6796cadd66588e41c
Remarc Balisi <=>
2019-01-10 03:24:01 +0800

    add sample query

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,33 @@
 |
 */
 use App\User;
+use App\Role;
+
 Route::get('/', function () {
     // return User::where(['id'=>Auth::user()->id])->first()->user_status()->where(['status_id'=>1])->get();
-    return view('welcome');
+    // return view('welcome');
+
+    $auth_user = Auth::user();
+
+    $roles = Role::where([
+        'id' => 1
+    ])->get();
+
+    $array_of_users = [];
+
+    foreach( $roles as $role ){
+        return $role->user_roles()->get();
+        foreach( $role->user_roles()->get() as $user_role ){
+            if( $auth_user->department_id == $user_role->user->first()->department_id ){
+                array_push($array_of_users, $user_role->user);
+            }
+            // array_push($array_of_users, $user_role->user);
+            // return $user_role->user;
+        }
+    }
+
+    return $array_of_users;
+
 });
 
 Auth::routes();
