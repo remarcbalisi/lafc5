037cd1bf3604fd39c0ce447ea04199cabfabc5ce
Remarc Balisi <=>
2019-01-10 05:09:15 +0800

    sending email for new user  registration: OK

--- config/mail.php
+++ config/mail.php
@@ -120,4 +120,16 @@ return [
         ],
     ],
 
+    /**
+     * Insecure Email, remove this for production
+     */
+
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+        ],
+    ],
+
 ];
