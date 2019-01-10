fcf31466a56cde05b6be9edd138e0bece133e775
Remarc Balisi <=>
2019-01-11 06:36:26 +0800

    team leader user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,5 +24,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AddLeaveStatusValues::class);
         $this->call(AddAgentUser::class);
         $this->call(AddLeaveTypeValues::class);
+        $this->call(AddTeamLeaderUser::class);
     }
 }
