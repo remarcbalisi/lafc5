ed3af909653f088698aa8516184c6711aaad7e83
Remarc Balisi <=>
2019-01-11 04:02:47 +0800

    add view single user  route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use App\Role;
 Route::get('/', function () {
     // return User::where(['id'=>Auth::user()->id])->first()->user_status()->where(['status_id'=>1])->get();
     // return view('welcome');
-    
+    // $user->hasRole($super_admin);
+    $super_admin = Role::where(['id' => 2])->first();
+    return Auth::user()->hasRole($super_admin) ? 'True' : 'False';
     $auth_user = Auth::user();
     $auth_user->roles()->where(['role_id'=>1])->first();
 
@@ -48,4 +50,5 @@ Route::middleware('auth', 'user_stat', 'admin_acc')->group(function () {
     Route::get('/home', 'HomeController@index')->name('home');
     Route::get('/user-lists', 'UserController@lists')->name('user-lists');
     Route::get('/leave-lists', 'UserController@lists')->name('leave-lists');
+    Route::get('/view-user/{user_id}', 'UserController@viewSingleUser')->name('view-single-user');
 });
