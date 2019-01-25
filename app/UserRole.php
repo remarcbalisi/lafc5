89260749259015782624786084a0e65a4123d790
Remarc Balisi <=>
2019-01-26 02:07:37 +0800

    add softdelete in user role

--- app/UserRole.php
+++ app/UserRole.php
@@ -3,9 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserRole extends Model
-{
+{   
+    use SoftDeletes;
+    
+    protected $dates = ['deleted_at'];
+
     protected $table = 'user_role';
 
     protected $fillable = [
