7438bf5c9677dc4ed00108b8cd0a35a248f79615
Regine Laguting <reginelaguting091998@gmail.com>
2019-02-01 23:49:54 +0800

    change table css

--- resources/views/userlists-sample.blade.php
+++ resources/views/userlists-sample.blade.php
@@ -8,124 +8,90 @@
 <div class="black-bar"></div>
 <div class="bg"><div>
 
-
-<table id="user-table">
-  <tr>
-    <th>Employee ID</th>
+<table class="greenTable">
+<thead>
+<tr>
+<th>Employee ID</th>
     <th>Name</th>
     <th>Status</th>
     <th></th>
-  </tr>
-  <tr>
-    <td>123-regine</td>
-    <td>Maria Anders</td>
-    <td>Active</td>
-    <td>
-    <div class="dropdown">
+</tr>
+</thead>
+<tfoot>
+<tr>
+<td colspan="4">
+<div class="links"><a href="#">&laquo;</a> <a class="active" href="#">1</a> <a href="#">2</a> <a href="#">3</a> <a href="#">4</a> <a href="#">&raquo;</a></div>
+</td>
+</tr>
+</tfoot>
+<tbody>
+<tr>
+  <td>cell1_1</td>
+  <td>cell2_1</td>
+  <td>cell3_1</td>
+  <td>
+   <div class="dropdown">
         <a href="#" class="dropbtn">
           <span class="glyphicon glyphicon-option-vertical"></span>
-        </a>
-        <!-- <button class="dropbtn">Dropdown</button> -->
+        </a>  
     <div class="dropdown-content">
-    <a href="#">View</a>
-    <a href="#">Edit</a>
-    <a href="#">Suspend</a>
+        <a href="#"><span class="glyphicon glyphicon-pencil"></span>&nbsp;View</a>
+        <a href="#"><span class="glyphicon glyphicon-eye-open"></span>&nbsp;Edit</a>
+        <a href="#"><span class="glyphicon glyphicon-exclamation-sign"></span>&nbsp;Suspend</a>
     </div>
   </div>
   </td>
-  </tr>
-  <tr>
-    <td>345-remarc</td>
-    <td>Christina Berglund</td>
-    <td>Active</td>
-    <td>
-        <a href="#">
-          <span class="glyphicon glyphicon-option-vertical"></span>
-        </a>
-    </td>
-  </tr>
-  <tr>
-    <td>22-Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Active</td>
-    <td>
-        <a href="#">
-          <span class="glyphicon glyphicon-option-vertical"></span>
-        </a>
-    </td>
-  </tr>
-  <tr>
-    <td>444-Handel</td>
-    <td>Roland Mendel</td>
-    <td>Active</td>
-    <td>
-        <a href="#">
-          <span class="glyphicon glyphicon-option-vertical"></span>
-        </a>
-    </td>
-  </tr>
-  <tr>
-    <td>222-Trading</td>
-    <td>Helen Bennett</td>
-    <td>Active</td>
-    <td>
-        <a href="#">
-          <span class="glyphicon glyphicon-option-vertical"></span>
-        </a>
-    </td>
-  </tr>
-  <tr>
-    <td>345-Essen</td>
-    <td>Philip Cramer</td>
-    <td>Active</td>
-    <td>
-        <a href="#">
-          <span class="glyphicon glyphicon-option-vertical"></span>
-        </a>
-    </td>
-  </tr>
-  <tr>
-    <td>121-Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Active</td>
-    <td>
-        <a href="#">
-          <span class="glyphicon glyphicon-option-vertical"></span>
-        </a>
-    </td>
-  </tr>
-  <tr>
-    <td>101-Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Active</td>
-    <td>
-        <a href="#">
-          <span class="glyphicon glyphicon-option-vertical"></span>
-        </a>
-    </td>
-  </tr>
-  <tr>
-    <td>456-South</td>
-    <td>Simon Crowther</td>
-    <td>Active</td>
-    <td>
-        <a href="#">
-          <span class="glyphicon glyphicon-option-vertical"></span>
-        </a>
-    </td>
-  </tr>
-  <tr>
-    <td>88-spécialités</td>
-    <td>Marie Bertrand</td>
-    <td>Active</td>
-    <td>
-        <a href="#">
-          <span class="glyphicon glyphicon-option-vertical"></span>
-        </a>
-    </td>
-  </tr>
+</tr>
+<tr>
+<td>cell1_2</td
+><td>cell2_2</td>
+<td>cell3_2</td>
+<td><span class="glyphicon glyphicon-option-vertical"></span></td>
+</tr>
+<tr>
+<td>cell1_3</td>
+<td>cell2_3</td>
+<td>cell3_3</td>
+<td><span class="glyphicon glyphicon-option-vertical"></span></td>
+</tr>
+<tr>
+<td>cell1_4</td>
+<td>cell2_4</td>
+<td>cell3_4</td>
+<td><span class="glyphicon glyphicon-option-vertical"></span></td></tr>
+<tr>
+<td>cell1_5</td>
+<td>cell2_5</td>
+<td>cell3_5</td>
+<td><span class="glyphicon glyphicon-option-vertical"></span></td></tr>
+<tr>
+<td>cell1_6</td>
+<td>cell2_6</td>
+<td>cell3_6</td>
+<td><span class="glyphicon glyphicon-option-vertical"></span></td></tr>
+<tr>
+<td>cell1_7</td>
+<td>cell2_7</td>
+<td>cell3_7</td>
+<td><span class="glyphicon glyphicon-option-vertical"></span></td></tr>
+<tr>
+<td>cell1_8</td>
+<td>cell2_8</td>
+<td>cell3_8</td>
+<td><span class="glyphicon glyphicon-option-vertical"></span></td></tr>
+<tr>
+<td>cell1_9</td>
+<td>cell2_9</td>
+<td>cell3_9</td>
+<td><span class="glyphicon glyphicon-option-vertical"></span></td></tr>
+<tr>
+<td>cell1_10</td>
+<td>cell2_10</td>
+<td>cell3_10</td>
+<td><span class="glyphicon glyphicon-option-vertical"></span></td></tr>
+</tbody>
+</tr>
 </table>
-
 
 </div>
 </div>
