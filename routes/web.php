31f69345908af8b7d33ebe81c768381fcde69955
Remarc Balisi <remarc.balisi@gmail.com>
2019-02-14 08:47:12 +0800

    change auth routes and integrate landing page.

--- routes/web.php
+++ routes/web.php
@@ -89,9 +89,23 @@ Route::get('/', function () {
 
 })->name('landing-page');
 
+// Authentication Routes...
 Route::get('/login', function () {
     return view('frontpage');
 })->name('login');
+
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
 
 
