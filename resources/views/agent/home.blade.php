c3de85ac14d5d1e70b37b907f7f12a970e6b6b91
Regine Laguting <reginelaguting091998@gmail.com>
2019-03-13 01:04:58 +0800

    edit agent page

--- resources/views/agent/home.blade.php
+++ resources/views/agent/home.blade.php
@@ -1,7 +1,73 @@
 @extends('agent.layouts.app')
 
 @section('content')
-    <div class="container">
+
+<div class="logo">
+<img src="/images/logo.png">
+</div>
+<div class="black-bar"></div>
+<div class="bg"><div>
+
+
+
+ <form class="form">
+      <h1></h1>
+
+      <div class="form-group" >
+      <label>Leave Type</label>
+      <select class="form-control input-lg">
+        <option>Sick Leave</option>
+        <option>Vacation Leave</option>
+        <option>3</option>
+      </select>
+      </div>
+
+      <div class="form-group">
+        <label>Date</label>
+        <input type="text" class="form-control  input-lg" name="datefilter" value="" />
+
+          <script type="text/javascript">
+          $(function() {
+
+            $('input[name="datefilter"]').daterangepicker({
+                autoUpdateInput: false,
+                locale: {
+                    cancelLabel: 'Clear'
+                }
+            });
+
+            $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
+                $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+            });
+
+            $('input[name="datefilter"]').on('cancel.daterangepicker', function(ev, picker) {
+                $(this).val('');
+            });
+
+          });
+          </script>
+      </div>
+
+      <div class="form-group">
+        <label>Address</label>
+        <input type="text" class="form-control input-lg">
+      </div>
+
+      <div class="form-group">
+      <label>Contact number</label>
+        <input type="number" class="form-control input-lg">
+      </div>
+
+      <div class="form-group">
+        <label for="comment">Note:</label>
+        <textarea class="form-control" rows="5" id="comment"></textarea>
+    </div>
+
+      <button type="submit" >Apply</button>
+  </form>
+
+
+    <!-- <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
@@ -19,5 +85,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 @endsection
--- resources/views/agent/users/view.blade.php
+++ resources/views/agent/users/view.blade.php
@@ -1,7 +1,73 @@
 @extends('agent.layouts.app')
 
 @section('content')
-    <div class="container">
+
+
+<div class="logo">
+<img src="/images/logo.png">
+</div>
+<div class="black-bar"></div>
+<div class="bg"><div>
+
+
+@if (session('status'))
+    <div class="alert alert-success">
+        {{ session('status') }}
+    </div>
+@endif
+
+<header>
+  <div style="padding-left:10%">
+
+    <h2 style="padding-top:3%;font-weight:bold;">
+    {{ $user->fname . ' ' . $user->lname }} (
+                            @foreach( $user->user_status()->get() as $u_status )
+                                {{$u_status->status->name . ', '}}
+                            @endforeach
+                                         )
+    </h2>
+    <br>
+    <h4 style="margin-left:6%"><strong>{{$user->email}}</strong></h4>
+
+    
+    <h4 style="margin-left:6%">
+    <br>
+    <p><strong>Gender:</strong> {{ $user->gender->name }}</p>
+    </h4>
+    <br>
+    <h4 style="margin-left:6%"> 
+    <strong>Position:</strong>
+    @foreach( $user->user_roles()->get() as $user_role )
+        {{ $user_role->role->name . ', ' }}
+    @endforeach
+    </h4>
+    <br>
+    @foreach( $user->contacts()->get() as $contact )
+    <h4 style="margin-left:6%"><strong>Contact:</strong>  
+    {{ $contact->contact_type->name . ': ' . $contact->number }}
+    </h4>
+    @endforeach
+    <br>
+    @foreach( $user->address()->get() as $addr )
+    <h4 style="margin-left:6%"><strong> Address:</strong>
+    {{ $addr->street . ' ' . $addr->unit_no . ' blah blah..' }}
+    </h4>                         
+    @endforeach
+    <br>
+    <h4 style="margin-left:6%"><strong>Department:</strong> {{ $user->department->name }}</h4>
+    </div>
+</header>
+<section>
+  <h1 class="bottom"></h1>
+</section>
+<a href="{{route('edit-user', ['user_id'=>$user->id])}}"><button type="button" class="btn btn-default btn-circle btn-xl">
+    <i class="glyphicon glyphicon-pencil"></i></button></a>
+</div>
+</div>
+
+
+
+    <!-- <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
@@ -75,5 +141,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 @endsection
