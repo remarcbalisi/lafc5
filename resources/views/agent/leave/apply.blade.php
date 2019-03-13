7b5d575e7d7bd0fad8161d0ae24ff5429342ed30
Remarc Balisi <remarcbalisi@gmail.com>
2019-03-13 08:28:54 +0800

    add checking

--- resources/views/agent/leave/apply.blade.php
+++ resources/views/agent/leave/apply.blade.php
@@ -97,7 +97,7 @@ button{
       <div class="form-group">
         <label>Address</label>
         <input type="text" class="form-control input-lg" type="text" name="leave_address" 
-        value="{{!empty($user->concatAddress()[0]) ? $user->concatAddress()[0] : 'N/A'}}" class="form-control" id="leave_address" placeholder="Address">
+        value="{{!empty($user->concatAddress()) ? $user->concatAddress()[0] : 'N/A'}}" class="form-control" id="leave_address" placeholder="Address">
       </div>
 
       <div class="form-group">
