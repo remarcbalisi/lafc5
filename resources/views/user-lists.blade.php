4454c7546fe464b4bbe971d72c20c3e1a4e6da33
Regine Laguting <reginelaguting091998@gmail.com>
2019-03-13 16:22:02 +0800

    edit user list page

--- resources/views/user-lists.blade.php
+++ resources/views/user-lists.blade.php
@@ -1,7 +1,119 @@
 @extends('layouts.app-copy')
 
 @section('content')
-<div class="container">
+<style>
+.container{
+  margin-top:5%;
+}
+</style>
+
+
+<div class="logo">
+  <img src="/images/logo.png">
+</div>
+<div class="black-bar"></div>
+<div class="bg"><div>
+
+
+<div class="reportwrapper">
+  <div class="reportwrapper__role">
+    <p>User Lists</p>
+    <div class="grid-container">
+  </div>
+  </div>
+
+  <div class="reportwrapper__table">
+    <table border="0" cellspacing="0">
+      <thead>
+        <tr>
+          <th>
+            <p>Name</p>
+          </th>
+         
+          <th>
+            <p></p>
+          </th>
+          <th>
+            <p>Status</p>
+          </th>
+          <th>
+            <p style="margin-left:15%">Action</p>
+          </th>
+          
+          <th>
+            <p>&nbsp;</p>
+          </th>
+        </tr>
+      </thead>
+     
+      <tbody>
+        <tr class="t-row">
+        @foreach( $users as $user )
+          <td> 
+            <div class="item">
+              <div class="i">
+                <p class="name">{{$user->fname . ' ' . $user->lname}}</p>
+                <p class="designation">ID #: {{$user->employee_id}}</p>
+                <!-- <p class="designation">IT department</p>
+                <p class="location">lahug</p> -->
+              </div>
+            </div>
+          </td>
+          <td>
+          </td>
+          <td>
+          <div class="status">
+              <p status="status">
+               @foreach( $user->user_status()->get() as $u_status )
+                  {{$u_status->status->name . ', '}}
+                  @endforeach</p>
+            </div>
+          </td>
+          <td>
+            <div class="rate">
+            <a href="{{route('view-single-user', ['user_id'=>$user->id])}}">
+              <button type="button" class="btn btn-primary btn-sm">Preview</button>
+              </a>
+            
+              <a href="#"
+                    onclick="event.preventDefault();
+                                document.getElementById('user-status-form-{{$user->id}}').submit();">
+              <button type="button" class="btn btn-primary btn-sm">{{( $user->user_status()->where(['user_id'=>$user->id, 'status_id'=>3])->first() ) ? 'Unsuspend' : 'Suspend'}}</button>
+              </a>
+                <form id="user-status-form-{{$user->id}}" action="{{route('update-user-status')}}" method="POST">
+                    {{ csrf_field() }}
+                    {{ method_field('PUT') }}
+                    <input name="user_id" type="hidden" value="{{$user->id}}">
+                    <input name="status" type="hidden" value="{{( $user->user_status()->where(['user_id'=>$user->id, 'status_id'=>3])->first() ) ? 'unsuspend' : 'suspend'}}">
+                </form>
+            </div>
+          </td>
+          <td>
+          </td>
+          <!-- <td>
+            <div class="extra">
+            <div class="dropdown">
+            <a href="#" class="dropbtn">
+              <span class="glyphicon glyphicon-option-vertical"></span>
+            </a>  
+            <div class="dropdown-content">
+                <a href="#"><span class="glyphicon glyphicon-pencil"></span>&nbsp;View</a>
+                <a href="#"><span class="glyphicon glyphicon-eye-open"></span>&nbsp;Edit</a>
+                <a href="#"><span class="glyphicon glyphicon-exclamation-sign"></span>&nbsp;Suspend</a>
+            </div>
+          </div>
+            </div>
+          </td> -->
+        </tr>
+
+        @endforeach
+      </tbody>
+    </table>  
+</div>
+</div>
+
+
+<!-- <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -61,5 +173,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
