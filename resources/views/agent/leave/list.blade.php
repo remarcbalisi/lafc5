c6632c1acfb800790cbc4fb736a29718d1080cfc
Regine Laguting <reginelaguting091998@gmail.com>
2019-03-14 02:29:48 +0800

    edit hrm leave request list

--- resources/views/agent/leave/list.blade.php
+++ resources/views/agent/leave/list.blade.php
@@ -14,8 +14,77 @@
 <div class="bg"><div>
 
 
+<div class="reportwrapper">
+  <div class="reportwrapper__role">
+    <p>My Leave Lists</p>
+    <div class="grid-container">
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
+  </div>
+  </div>
 
-    <div class="container">
+  <div class="reportwrapper__table">
+    <table border="0" cellspacing="0">
+      <thead>
+        <tr>
+          <th>
+            <p>Range</p>
+          </th>
+          <th>
+            <p>Status</p>
+          </th>
+          <th>
+           
+          </th>
+
+          <th>
+
+          </th>
+          
+          <th>
+            <p>Action</p>
+          </th>
+        </tr>
+      </thead>
+     
+      <tbody>
+      @foreach($leave_requests as $leave_request)
+        <tr class="t-row">
+          <td> 
+            <div class="item">
+              <div class="i">
+                <p class="name">{{$leave_request->leave->dateFormat($leave_request->leave_start_date)}} - {{$leave_request->leave->dateFormat($leave_request->leave_end_date)}}</p>
+                <p class="designation">ID #: {{$leave_request->id}}</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div class="status">
+              <p status="status">{{$leave_request->leave->leave_status->name}}</p>
+            </div>
+          </td>
+          <td>
+           
+          </td>
+
+          <td>
+        
+          </td>
+
+          <td>
+          <a href="{{route('agent-leave-view', ['leave_request_id'=>$leave_request->leave->id])}}">
+          <button type="button" class="btn btn-info btn-md">View</button></a>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+</div>
+</div>
+
+    <!-- <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
@@ -55,5 +124,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 @endsection
