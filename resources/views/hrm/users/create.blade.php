a258e68e07eaf86839759d7acf277b65052ba718
Regine Laguting <reginelaguting091998@gmail.com>
2019-03-14 02:31:09 +0800

    edit css

--- resources/views/hrm/users/create.blade.php
+++ resources/views/hrm/users/create.blade.php
@@ -2,11 +2,6 @@
 
 @section('content')
 
-<style>
-.container{
-    margin-top:5%;
-}
-</style>
 
 <div class="logo">
 <img src="/images/logo.png">
@@ -15,7 +10,7 @@
 <div class="bg"><div>
 
 
-    <div class="container">
+    <div class="container" style="margin-top:5%">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
